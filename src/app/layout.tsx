817748ed707367904a0fc03d6a35588a0c3e61bd
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-12 15:27:58 +0900

    add modern normalize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "modern-normalize/modern-normalize.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
